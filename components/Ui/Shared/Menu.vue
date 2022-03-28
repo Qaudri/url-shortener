8d68e84ede1d05bdd88d3d88fafa57abd0573074
Qaudri <aquadirmuhammad@gmail.com>
2022-03-28 18:35:52 +0100

    chore: menu component style completed

--- components/Ui/Shared/Menu.vue
+++ components/Ui/Shared/Menu.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="flex items-center">
     <UiButtonsSecondary button_title="Login" class="mx-6" />
     <UiButtonsPrimary button_title="Sign Up" class="mx-6" />
   </div>
