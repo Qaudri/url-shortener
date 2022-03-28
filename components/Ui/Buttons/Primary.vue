55f03adb540a4897c9a85dde5510c59eb35db06b
Qaudri <aquadirmuhammad@gmail.com>
2022-03-28 19:00:16 +0100

    refactor: primary button border radius changed

--- components/Ui/Buttons/Primary.vue
+++ components/Ui/Buttons/Primary.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <button class="px-6 py-2 text-lg text-black hover:text-white font-medium hover:bg-primary-100 border-2 
-    hover:border-primary-100 border-black rounded-lg duration-500 ease-in-out">
+    hover:border-primary-100 border-black rounded-2xl duration-500 ease-in-out">
       {{button_title}}
     </button>
   </div>
