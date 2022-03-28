f8c231a7dafac0a9ecc3c1c5dd6ed18f49ba0d8a
Qaudri <aquadirmuhammad@gmail.com>
2022-03-28 18:04:16 +0100

    chore: logo component created

---
+++ components/Ui/Shared/Company-logo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <a href="/">
+      <img :src="require('@/assets/images/logo.svg')" alt="">
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
