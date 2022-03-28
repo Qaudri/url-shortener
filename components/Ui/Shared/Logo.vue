486688f5913e4b10060cbdaa760351cbfe284f14
Qaudri <aquadirmuhammad@gmail.com>
2022-03-28 18:54:38 +0100

    refactor: logo width changed

--- components/Ui/Shared/Logo.vue
+++ components/Ui/Shared/Logo.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <a href="/">
-      <img :src="require('@/Assets/images/logo.svg')" alt="">
+      <img :src="require('@/Assets/images/logo.svg')" alt="" class="w-28">
     </a>
   </div>
 </template>
