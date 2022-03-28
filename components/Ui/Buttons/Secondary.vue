2c0a6490d239f36ab366b2554e16d584239928df
Qaudri <aquadirmuhammad@gmail.com>
2022-03-28 18:24:55 +0100

    secondary button title prop created:

---
+++ components/Ui/Buttons/Secondary.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button>
+      {{button_title}}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    button_title: String
+  }
+}
+</script>
+
+<style>
+
+</style>
