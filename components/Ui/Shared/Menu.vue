cfdc5d670080ad39534f13ea330b6362e2746f3f
Qaudri <aquadirmuhammad@gmail.com>
2022-03-28 18:34:54 +0100

    chore: menu component created

---
+++ components/Ui/Shared/Menu.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <UiButtonsSecondary button_title="Login" />
+    <UiButtonsPrimary button_title="Sign Up" />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
