d3874582456a5de8f22e665ad187bead4e0f9479
Qaudri <aquadirmuhammad@gmail.com>
2022-03-28 18:18:47 +0100

    chore: primary button component created

---
+++ components/Ui/Buttons/Primary-btn.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <button class="px-4 py-2 text-lg text-black hover:text-white font-medium hover:bg-primary-100 border-2 
+    border-primary-100 rounded-lg duration-500 ease-in-out">
+      {{button_title}}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    button_title: String
+  }
+}
+</script>
+
+<style>
+
+</style>
