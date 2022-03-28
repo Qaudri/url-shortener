fae7ad1db6180e479b948f1d3be26136b6dec66f
Qaudri <aquadirmuhammad@gmail.com>
2022-03-28 18:56:31 +0100

    refactor: primary button refactored

--- components/Ui/Buttons/Primary.vue
+++ components/Ui/Buttons/Primary.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <button class="px-4 py-2 text-lg text-black hover:text-white font-medium hover:bg-primary-100 border-2 
-    border-primary-100 rounded-lg duration-500 ease-in-out">
+    <button class="px-6 py-2 text-lg text-black hover:text-white font-medium hover:bg-primary-100 border-2 
+    hover:border-primary-100 border-black rounded-lg duration-500 ease-in-out">
       {{button_title}}
     </button>
   </div>
