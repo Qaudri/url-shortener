bfe53d0ff486d9710c5ce38b6b392c9223dab34e
Qaudri <aquadirmuhammad@gmail.com>
2022-03-29 14:24:44 +0100

    chore: dashboard icons added

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,5 +1,55 @@
 <template>
-  
+  <div>
+    <SectionsCommonHeader class="absolute w-full" />
+    <div class="flex">
+      <div class="w-1/5 bg-primary-100 bg-opacity-5 h-screen border-r-2 border-purple-600 flex items-center">
+
+        <div class="w-full">
+          <div class="flex items-center py-2 border-b-2 border-purple-600 w-full my-2">
+          <div>
+
+          </div>
+          <div>
+            <h1 class="font-bold text-5xl text-black my-1">
+              0
+            </h1>
+            <p class="font-normal text-base text-gray-400">Total Clicks</p>
+          </div>
+          </div>
+
+          <div class="flex items-center py-2 border-b-2 border-purple-600 w-full my-2">
+            <div>
+
+            </div>
+            <div>
+              <h1 class="font-bold text-5xl text-black my-1">
+                0
+              </h1>
+              <p class="font-normal text-base text-gray-400">Top referrer</p>
+            </div>
+          </div>
+
+          <div class="flex items-center py-2 border-b-2 border-purple-600 w-full my-2">
+            <div>
+
+            </div>
+            <div>
+              <h1 class="font-bold text-5xl text-black my-1">
+                0
+              </h1>
+              <p class="font-normal text-base text-gray-400">Top location</p>
+            </div>
+          </div>
+
+        </div>
+
+
+      </div>
+      <div class="w-3/5">
+
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
