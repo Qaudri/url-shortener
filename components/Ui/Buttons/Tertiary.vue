e1b114038d86080384aa3fbd6f2ad7d1d1fc1331
Qaudri <aquadirmuhammad@gmail.com>
2022-03-30 11:49:33 +0100

    chore: tertiary button design completed

---
+++ components/Ui/Buttons/Tertiary.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="text-white uppercase font-medium bg-primary-100 px-4 rounded-lg py-2">
+    {{button_text}}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    button_text: String
+  }
+}
+</script>
+
+<style>
+
+</style>
