8b0f80766a753074736e762c1b22b68e5f8a1efd
Qaudri <aquadirmuhammad@gmail.com>
2022-03-30 15:01:07 +0100

    chore: register page created

---
+++ pages/signup.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="relative container mx-auto">
+    <SectionsCommonHeader class="absolute top-0 w-full" />
+    <div class="flex h-screen justify-center items-center">
+      <UiFormLogin />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
