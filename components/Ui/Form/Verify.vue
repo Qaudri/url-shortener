6dcd9c9b1c611adcc4dff67b9e9c174bbcb6496e
Qaudri <aquadirmuhammad@gmail.com>
2022-03-30 15:28:47 +0100

    chore: email verification form component build completed

---
+++ components/Ui/Form/Verify.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="w-full mx-12 md:w-1/2 lg:w-2/5  border-primary-100 border-2 py-8 px-6 rounded-2xl">
+
+    <form action="" class="block" @submit.prevent="submitForm">
+
+      <label for="username" class="block text-lg text-primary-100 font-medium">Verify your email</label>
+      <div class="py-1"></div>
+      <input type="email" v-model="form.username" placeholder="Enter the 6 digit verification code" 
+      class="block w-full py-2 px-4 rounded-lg bg-primary-100 bg-opacity-5 focus:border-primary-100">
+
+      <UiButtonsTertiary button_text="Verify Email" class="bg-opacity-5 mt-10 text-xl text-black font-medium py-3 flex justify-center bg-primary-100 w-full" />
+
+    </form>
+    
+  </div>
+  
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
