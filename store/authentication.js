a7001a0695f2598516656f586d662ee672535768
Qaudri <aquadirmuhammad@gmail.com>
2022-03-31 13:24:56 +0100

    chore: new action state and mutation added for user info

--- store/authentication.js
+++ store/authentication.js
@@ -2,6 +2,9 @@
 export const state = () => ({
   session_token: process.browser ? localStorage.getItem('ubit_user_session_token') : null || null,
   auth_status: false,
+  user: {
+
+  },
 })
 
 export const getters = {
@@ -53,6 +56,20 @@ export const actions = {
         })
     })
   },
+
+  retrieveLoggedUserInfo(context, credentials) {
+    return new Promise((resolve, reject) => {
+      this.$axios.$get('/api/user/account')
+        .then(response => {
+          context.commit('SET_USER_INFO', response.data)
+          resolve(response)
+        })
+
+        .catch(function (error) {
+          reject(error)
+        })
+    })
+  },
 }
 
 export const mutations = {
@@ -62,5 +79,9 @@ export const mutations = {
 
   SET_AUTHENTICATION_STATUS(state, payload){
     state.auth_status = payload
+  },
+
+  SET_USER_INFO(state, payload){
+    state.user = payload
   }
 }
