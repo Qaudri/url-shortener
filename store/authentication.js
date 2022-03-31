f630ca05aea67af804fe4cea166fefb23a82afc6
Qaudri <aquadirmuhammad@gmail.com>
2022-03-31 13:56:13 +0100

    chore: email verification middleware updated

--- store/authentication.js
+++ store/authentication.js
@@ -59,6 +59,8 @@ export const actions = {
 
   retrieveLoggedUserInfo(context, credentials) {
     return new Promise((resolve, reject) => {
+      this.$axios.defaults.headers.common['Authorization'] = 'Bearer ' + context.state.session_token
+
       this.$axios.$get('/api/user/account')
         .then(response => {
           context.commit('SET_USER_INFO', response.data)
